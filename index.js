0fbc59552c6002f422372bfdf9bea74e383ecb93
NKN1396 <someone.took.nkn1396@gmail.com>
2020-10-05 13:29:03 +0200

    Made shareable config

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = {
+	env: {
+		node: true,
+		es2020: true
+	},
+	extends: [
+		"eslint:recommended",
+		"standard"
+	],
+	parserOptions: {
+		sourceType: "module"
+		//ecmaVersion already gets set by env.es2020
+	},
+	
+	rules: {
+		"no-console": [ "off" ],
+
+		indent: [ "error", "tab", { "SwitchCase": 1 } ],
+		"linebreak-style": [ "error", "unix" ],
+		quotes: [ "error", "double" ],
+		semi: [ "error", "never" ],
+		"eol-last": [ "error", "always" ],
+		"no-multiple-empty-lines": [ "error", { max: 2 } ],
+	}
+}
